64de76b2d652954263fb5974af917827b5526f19
EloiPellin <epm@holacons.com>
2024-06-21 01:22:38 +0200

    Added core html & styles/i18n config closes #2 #5 #6

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  public languages = [
+    {
+      value: "en",
+      alt: "English",
+      flag: "./../../../assets/images/en-flag.png"
+    },
+    {
+      value: "es",
+      alt: "Español",
+      flag: "./../../../assets/images/es-flag.png"
+    }
+  ];
+  private translateService = inject(TranslateService);
+
+  getLanguage(language:string){
+    this.translateService.use(language);
+  }
+
 
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-
-
+import { throwError } from 'rxjs';
+import { TranslateModule } from '@ngx-translate/core';
+import { provideTranslation } from './config/i18n/translate-loader.config';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,19 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule.forRoot(provideTranslation())
+  ],
+  exports:[
+    HeaderComponent,
+    FooterComponent,
+    TranslateModule
   ]
 })
-export class CoreModule { }
+export class CoreModule { 
+   constructor(@Optional() @SkipSelf() parentModule:CoreModule){
+    if(parentModule){
+      throw new Error("CoreModule is already loaded");
+    }
+   }
+}
